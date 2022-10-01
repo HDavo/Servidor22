b64c7693d9d8f525a5fc9391c5525f0019ea8696
HDavo <dabghos@outlook.com>
2022-10-01 17:58:30 +0200

    Cambio en ejercicios

--- Ejercicios/tema0/matriz100.php
+++ Ejercicios/tema0/matriz100.php
@@ -1,6 +1,7 @@
 <?php
     $num_filas=10;
     $num_columnas = $num_filas;
+    $n=0;
 
     //para definir funciones dentro de php se debe poner siempre function antes de la función que queremos definir
     function esPrimo($n) {
@@ -13,6 +14,7 @@
                 if (($n % $c) == 0)  {
                     $esprimo = false;
                 }
+                $c++;
             }
             return $esprimo;
         }
@@ -26,6 +28,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Matriz de 100 números</title>
+    <link rel="stylesheet" href="piramide_estilo.css">
     <!-- poner el css como externo en base a las características que le ha puesto el a la tabla -->
 </head>
 <body>
@@ -33,9 +36,11 @@
         <?php for ($i=0; $i < $num_filas; $i++) { ?>
             <!-- con esto se hace que tengamos un bucle que genere tantas filas como le indique el bucle -->
             <tr>
-                <?php for ($j=0; $j < $num_columnas; $j++) { ?>
-                    <td <?php if(esPrimo($i*10+$j)) ><?=$i.$j?></td>
-                <?php } ?>            
+                <?php for ($j = 0; $j < $num_columnas; $j++) {
+                    $num_s = $i*10+$j;
+                    ?>
+                    <td <?= (esPrimo($num_s))?'class="esPrimo"':''?>><?=$num_s?></td>    
+                <?php } ?>          
             </tr>
         <?php } ?>
 
--- Ejercicios/tema0/tabla.php
+++ Ejercicios/tema0/tabla.php
@@ -1,32 +1,64 @@
 <?php
-
+    //aqui elegimos el número del que obtendremos la tabla de multiplicar
+    //también elegimos hasta que número queremos multiplicar
     $num = 7;
     $hasta = 10;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <table>
-        <thead>
-            <tr>
-                <th>Tabla del <?=$num?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php for ($i=0; $i < $hasta; $i++) { ?>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Tabla de multiplicar</title>
+        <style>
+            *{
+                margin: 0;
+                padding: 0;
+            }
+
+            #general{
+
+                width: 80vw;
+                height: 100vh;
+                margin: auto;
+            }
+
+            table {
+                width: 50%;
+                height: 80%;
+                margin: 0 auto;
+
+            }
+
+            tr, td {
+                background-color: grey;
+                color: white;
+                text-align: center;
+                padding: 2%;
+
+            }
+        </style>
+    </head>
+    <body>
+        <div id="general">
+        <table>
+            <thead>
                 <tr>
-                    <td><?=$num?> x <?=$i?></td>
-                    <td><?=$num*$i?></td>
+                    <th colspan="2">Tabla del <?=$num?></th>
                 </tr>
-            <?php } ?>
-        </tbody>
-    </table>
-</body>
+            </thead>
+            <tbody>
+                <?php for ($i=0; $i < $hasta; $i++) { ?>
+                    <tr>
+                        <td><?=$num?> x <?=$i?></td>
+                        <td><?=$num*$i?></td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
+        </div>
+
+    </body>
 </html>
