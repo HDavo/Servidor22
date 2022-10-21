14015c39b8707f19e4237683159d6e2c6c7292cf
HDavo <99563047+HDavo@users.noreply.github.com>
2022-10-21 12:48:14 +0000

    Update CuentaBancaria.php

--- Ejercicios/tema4/Objetos/CuentaBancaria.php
+++ Ejercicios/tema4/Objetos/CuentaBancaria.php
@@ -40,10 +40,42 @@
         }
 
         public function impresion(){
-            return "Numero de cuenta: ".$this->numeroCuenta." Saldo: ".$this->saldo."<br>";
+            return "Titular: ". $nombre ."Numero de cuenta: ".$this->numeroCuenta." Saldo: ".$this->saldo."<br>";
         }
+
+        
 
         //método para realizar ingresos
         
+        public function ingreso(int $cantidadIngresada){
+            $this->saldo+=$cantidadIngresada;
+            return "Su saldo actual es de $saldo.";
+        }
+
+        public function retirada(int $cantidadRetirada){
+            $this->saldo-=$cantidadRetirada;
+            return "Su saldo actual es de $saldo.";
+        }
+
+        public function transferencia(int $cantidadTransferida, int $cuentaDestino) {
+            $this->saldo= $saldo - $cantidadTransferida;
+            $this->$numeroCuenta=$cuentaDestino;
+
+            $numeroCuenta->ingreso($cantidadTransferida);
+
+            return "Se han transferido  $cantidadTransferida euros a la cuenta $cuentaDestino. <br> Su saldo disponible es de $saldo."
+        }
+
+        public function fusion(int $cuenta){
+            $this->saldo += $cuenta->saldo;
+            $cuenta->saldo = 0;
+            $numeroCuenta->$numeroCuenta-1;
+
+            return "Se ha realizado el traspaso del saldo de la cuenta número $cuenta a su cuenta con una cantidad correspondiente a $cuenta->saldo. <br> Su saldo total es de $saldo."
+        }
+
+        public function bancarrota(){
+            $this->saldo=0;
+        }
     }
 ?>
