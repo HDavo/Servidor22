d8487c25fca22c3e3f0cc4e33338c08376ea3dd3
Dav <dabghos@outlook.com>
2023-01-12 22:28:37 +0100

    Avances

--- ejercicios/tema4_objetos/avanzados/singleton.php
+++ ejercicios/tema4_objetos/avanzados/singleton.php
@@ -1,19 +1,29 @@
 <?php
 
-class Singleton {
-    private static $instance = null;
+    class Singleton {
+        //para usarlo en otras cosas es necesario poner un require
+        private $nombre;
 
+        private static $config;
 
-    private function __construct (){
-
-    }
-
-    public function getInstance(){
-        if(self::$instanceinstance==null){
-            self::$instance = new Singleton();
+        public static function singleton(){
+            //en el caso de que no exista un $config se crea mediante singleton, en el caso de que exista se devuelve el ya existente
+            if(!isset(self::$config)){
+                self::$config = new Singleton();
+            }
+            return self::$config;
         }
 
-        return self::$instance;
+        //el constructor debe ser privado para evitar que se pueda volver a instanciar
+
+        private function __construct(){}
+
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function getNombre(){
+            return $this->nombre;
+        }
     }
-}
 ?>
