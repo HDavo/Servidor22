4ded8b4cda315ff9acd5b430928de7dbd1174dea
Dav <dabghos@outlook.com>
2023-01-13 20:48:12 +0100

    foro ya funcional con correo

--- ejercicios/tema8_sesiones/foro_profesor/public/mail.php
+++ ejercicios/tema8_sesiones/foro_profesor/public/mail.php
@@ -1,6 +1,6 @@
 <?php
     require('../src/init.php');
 
-    Mailer::SendEmail("roman.kornyeyev1@educa.madrid.org", "Bienvenido","<h1>Este es el cuerpo</h1>");
+    Mailer::SendEmail($CONFIG['enviador'], "Bienvenido","<h1>Este es el cuerpo</h1>");
 
 ?>
