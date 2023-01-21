6fd9c2984a1d01b49cf40ba3ab08f0b977ffbcc9
Dav <dabghos@outlook.com>
2023-01-21 13:00:24 +0100

    Avances

--- ejercicios/tema8_sesiones/foro_profesor/public/edit.php
+++ ejercicios/tema8_sesiones/foro_profesor/public/edit.php
@@ -1,61 +1,56 @@
-<?php
+<?php 
+require("../src/init.php");
 
-    require("../src/init.php");
-    //se hace esto porque necesita autenticación
-    if(!isset($_SESSION['nombre']) || $_SESSION['nombre'] == ""){
-        header("Location: login.php?redirect=edit.php");
-        die();
-    }
- 
-    if(isset($_POST['actualizar'])){
-        //Actualizo todos los campos escritos, en este caso solo textarea
-        $DB->ejecuta(
-            "UPDATE usuarios
-            set descripcion = ?
-            WHERE id = ?",
-            $_POST['descripcion'],
-            $_SESSION['id']
-        );
+//se hace esto porque necesita autenticación
 
-        //Procesamiento de la imagen
+if(!isset($_SESSION['nombre']) || $_SESSION['nombre'] == "" ) {
+    header("Location: login.php?redirect=edit.php");
+    die();
+}
 
-        // print_r($_FILES);
-        // die();
-        
-        if(isset($_FILES['imagen'])){
-            $nombreTmp = $_FILES['imagen']['tmp_name'];
-            $nombre = $_FILES['imagen']['name'];
-            $tipo = $_FILES['imagen']['type'];
-
-            if($tipo == "image/png" || $tipo == "image/jpeg"){
-                if($_FILES['imagen']['error'] == 0){
-                    //mover el fichero a su posicion final
-                    move_uploaded_file($nombreTmp, "upload/perfiles/.".$_SESSION['id'].".png"); //la ruta deberia ir dentro de config
-
-                    $DB->ejecuta(
-                        "UPDATE usuarios SET img = ? WHERE id = ?",
-                        "uploads/perfiles/".$_SESSION['id'].".png",
-                        $_SESSION['id']
-                    );
-                }
-            }else{
-                //Mostrar error
-            }
-        }
-    }
-
-    //seria necesario mostrar los detalles anteriores con formato y luego los cambios que tengan lugar en ese mismo formato
-
+if(isset($_POST['actualizar'])){
+    // Actualizo todos los campos escritos, en este caso solo textarea
     $DB->ejecuta(
-        "SELECT *
-        FROM usuarios
-        WHERE id = ?",
+        "UPDATE usuarios SET descripcion = ? WHERE id = ?",
+        $_POST['descripcion'],
         $_SESSION['id']
     );
 
-    $usuario = $DB->obtenElDato();
-?>
+    // Proceso imagen
 
+    if(isset($_FILES['imagen'])) {
+        $nombreTmp = $_FILES['imagen']['tmp_name'];
+        $nombre = $_FILES['imagen']['name'];
+        $tipo = $_FILES['imagen']['type'];
+        if($tipo == "image/png" || $tipo == "image/jpeg") {
+            if($_FILES['imagen']['error'] == 0){
+                // Mover el fichero a su posición final
+                move_uploaded_file($nombreTmp, "upload/perfiles/".$_SESSION['id'].".png");
+                //la ruta donde se guardan los ficheros debería ir en config
+                // Actualizar la base de datos
+                $DB->ejecuta(
+                    "UPDATE usuarios SET img = ? WHERE id = ?",
+                    "upload/perfiles/".$_SESSION['id'].".png",
+                    $_SESSION['id']
+                );
+            }
+        } else {
+            // Mostrar error
+        }
+    }
+}
+
+    //seria necesario mostrar los detalles anteriores con formato y luego los cambios que tengan lugar en ese mismo formato
+
+$DB->ejecuta(
+    "SELECT *
+    FROM usuarios
+    WHERE id = ?",
+    $_SESSION['id']
+);
+$usuario = $DB->obtenElDato();
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -63,33 +58,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edición de perfil</title>
-    <style>
-        *{
-            margin: 0 auto;
-            padding: 0;
-        }
-
-        body{
-            background-color: papayawhip;
-        }
-    </style>
 </head>
 <body>
-    <h1>Aqui va el formulario para editar tu perfil</h1>
+    <h1>Aquí va el formulario para editar tu perfil</h1>
     <h3>Esta es tu información:</h3>
-    <h4>Bienvenido, <?=$usuario['nombre']?></h4><br>
-    <div>
-        <form action="" method="post" enctype="multipart/form-data">
-            <?php if($usuario['img'] != ""){ ?> 
-                <img src="<?=$usuario['img']?>" alt="foto_perfil"/> <br>
-            <?php } ?>
-            Imagen de perfil: <input type="file" accept="image/png,image/jpeg" name="imagen" id="imagen"><br>
-            <!-- Email: <input type="email"><br>  necesitaria enviar un correo de confirmación-->
-            Descripción: <br>
-            <textarea name="descripcion" id="" cols="30" rows="10"><?=$usuario['descripcion'] ?>
-            </textarea><br>
-            <input type="submit" value="Actualizar" name="actualizar">
-        </form>
-    </div>
+    <h4><?=$usuario['nombre']?></h4>
+    <h4>Esta es tu descripción actual: </h4>
+    <p><?=print($usuario['descripcion'])?></p>
+    <br>
+    <form action="" method="post" enctype="multipart/form-data">
+        <?php if($usuario['img'] != "") { ?>
+            <img src="<?=$usuario['img']?>" />
+        <?php } ?>
+        <br>
+        Imagen de perfil: <input type="file" accept="image/png,image/jpeg" name="imagen">
+        <br>
+        <!-- Email: <input type="email"><br>  necesitaria enviar un correo de confirmación-->
+        <textarea name="descripcion" id="" cols="30" rows="10" placeholder="Introduce tu descripción"></textarea><br>
+       
+        <br>
+        <input type="submit" value="Actualizar" name="actualizar">
+    </form>
 </body>
 </html>
--- ejercicios/tema8_sesiones/foro_profesor/public/listado.php
+++ ejercicios/tema8_sesiones/foro_profesor/public/listado.php
@@ -5,16 +5,44 @@
     $usuarios = $DB->obtenDatos();
 
 
+    $title = "Listado de usuarios";
+    $pageHeader = "Listado";
+    $pageId = "Listado";
+
+    $content = "Este es el contenido";
+
+    require("template.php");
+
+    //el controlador obtiene info del modelo
+    $DB->ejecuta("select * from usuarios");
+    $usuarios = $DB->obtenDatos();
+
+    //Se lo pasa a template (se puede establecer contenido personalizado para cada una de las páginas)
+    ob_start(); //mete el contenido especificado dentro deun buffer, por lo que no se muestra al usuario hasta que no se especifica
+
+    //meter tabla 
+    $content = ob_get_clean();
+
+
+    
+
+    /*<?php foreach($usuarios as $usuario) { ?>
+        <?php 
+            print_r($usuario);
+        ?>
+    <?php } ?> */
 ?>
 
-<!DOCTYPE html>
+<!-- lo siguiente nos viene dado por la plantilla -->
+
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?$CONFIG['title']?></title>
-    <style>
+    <style> 
         *{
             margin: 0 auto;
             padding: 0;
@@ -27,10 +55,6 @@
 </head>
 <body>
     <h1>Hola Mundo</h1>
-    <?php foreach($usuarios as $usuario) { ?>
-        <?php 
-            print_r($usuario);
-        ?>
-    <?php } ?>
+    
 </body>
-</html>
+</html> -->
