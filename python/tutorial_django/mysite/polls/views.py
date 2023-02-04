fe0dd443e332cb6559f1be643e481b955a10fcd7
Dav <dabghos@outlook.com>
2023-02-04 02:28:34 +0100

    Inicio Python

---
+++ python/tutorial_django/mysite/polls/views.py
@@ -0,0 +1,54 @@
+# from django.http import Http404
+from django.shortcuts import get_object_or_404,render
+
+# Estos dos se usan en el caso de no usar render
+
+from django.http import HttpResponse
+from django.template import loader
+
+
+from .models import Question
+
+# Create your views here.
+"""
+    index que se debe usar con HttpResponse
+    def index(request):
+    # return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list   
+    }
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
+"""
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+"""
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s. " % question_id) # ¿como printf de java?
+""" 
+""" 
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
+""" 
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results if question %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s. " % question_id)
+
