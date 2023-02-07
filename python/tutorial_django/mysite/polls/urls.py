320bbea8d2a7be11803221981fc642e2df10d1b6
Dav <dabghos@outlook.com>
2023-02-07 21:32:04 +0100

    Antes de plantillas

--- python/tutorial_django/mysite/polls/urls.py
+++ python/tutorial_django/mysite/polls/urls.py
@@ -2,11 +2,9 @@ from django.urls import path
 
 from . import views
 
-app_name = 'polls'
-urlpatterns = [ # en comentarios, ejemplos de las equivalencias
-    path('', views.index, name='index'), #/polls/
-    path('<int:pk>/', views.detail, name='detail'), # /polls/5/
-    path('<int:pk>/results/', views.Resultsview.as_view(), name='results'), # /polls/5/results/
-    path('<int:question_id>/vote/', views.vote, name='vote'), # /polls/5/vote/   
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
-
