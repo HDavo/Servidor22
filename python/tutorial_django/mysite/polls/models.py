037ec525a33460795853a20b54ee44e1241b752d
Dav <dabghos@outlook.com>
2023-02-11 19:20:00 +0100

    Test básico completo

--- python/tutorial_django/mysite/polls/models.py
+++ python/tutorial_django/mysite/polls/models.py
@@ -10,8 +10,15 @@ class Question(models.Model):
     pub_date = models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    # versión del método que no pasa los test
+"""     def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) """
+
+      
     
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
--- python/tutorial_django/mysite/polls/tests.py
+++ python/tutorial_django/mysite/polls/tests.py
@@ -1,3 +1,4 @@
+import datetime
 from django.test import TestCase
 from django.utils import timezone
 
@@ -8,13 +9,11 @@ class QuestionModelTests(TestCase):
     
     def test_was_published_recently_with_future_question(self):
         """
-        was_published_recently() returns False for questions whose pub_date
-        is in the future.
+        was_published_recently() returns False for questions whose
+        pub_date is in the future.
         """
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(pub_date=time)
         self.assertIs(future_question.was_published_recently(), False)
-
-
-    
-
+       
+       
