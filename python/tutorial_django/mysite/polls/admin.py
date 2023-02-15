ae8b70fb5d37411c79c76cd0514ca9e33127dea2
Dav <dabghos@outlook.com>
2023-02-15 20:50:11 +0100

    Antes de parte 6

--- python/tutorial_django/mysite/polls/admin.py
+++ python/tutorial_django/mysite/polls/admin.py
@@ -3,5 +3,31 @@ from django.contrib import admin
 from .models import Question, Choice
 # Register your models here.
 
+
+
+# from django.contrib import admin
+
+# from .models import Question
+
+
+""" 
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    
+    stackedInline ->
+    TabularInline -> 
+    
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [ # campos, es un array en donde se indican los campos que vamos a tener
+        (Principal,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+        
+    ]
+    
+   
+
+admin.site.register(Question, QuestionAdmin) """
+
 admin.site.register(Question)
 admin.site.register(Choice)
