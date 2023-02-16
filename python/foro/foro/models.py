02aac392a92c11d7b5647df447233b1825172795
Dav <dabghos@outlook.com>
2023-02-16 21:42:04 +0100

    Inicio foro 2

--- python/foro/foro/models.py
+++ python/foro/foro/models.py
@@ -9,8 +9,28 @@ class Usuario(models.Model):
     nombre_usuario = models.CharField(max_length=100)
     descripcion = models.TextField()
     fecha_registro = models.DateTimeField('fecha_registro')
-    foto_perfil = models.ImageField(upload_to='')
+    foto_perfil = models.ImageField(upload_to='f_perfil', null=False)
     
     
     def __str__(self):
-        return f"{self.nombre_usuario} ({self.descripcion})"
+        return f"{self.nombre_usuario} ({self.descripcion}) ({self.fecha_registro})"
+    
+    def getFoto(self):
+        return [self.foto_perfil]
+    
+
+class Hilo(models.Model):
+    creador = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    fecha_creacion = models.DateTimeField('fecha_creacion')
+    contenido = models.TextField()
+    imagen_hilo_1 = models.ImageField(upload_to='f_hilo', null=False)
+    imagen_hilo_2 = models.ImageField(upload_to='f_hilo', null=True)
+    imagen_hilo_3 = models.ImageField(upload_to='f_hilo', null=True)
+    
+    def __str__(self):
+        return f"{self.creador} ({self.titulo}) ({self.fecha_creacion})"
+
+class Comentario(models.Model):
+    autor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre_hilo = models.ForeignKey(Hilo, on_delete=models.CASCADE)
--- python/foro/foro/settings_publico.py
+++ python/foro/foro/settings_publico.py
@@ -132,3 +132,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Directorio para los archivos
+
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
