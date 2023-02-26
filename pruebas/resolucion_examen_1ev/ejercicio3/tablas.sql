7a4eabafd0c0b9d2d504fcc752f77ba7ed8f2514
Dav <dabghos@outlook.com>
2023-02-26 16:40:56 +0100

    Ejercicio BBDD

--- pruebas/resolucion_examen_1ev/ejercicio3/tablas.sql
+++ pruebas/resolucion_examen_1ev/ejercicio3/tablas.sql
@@ -1,3 +1,13 @@
+-- para iniciar la bbdd y las tablas necesarias para que el ejercicio funcione
+
+CREATE DATABASE examen; --NB BASE DE DATOS
+CREATE USER 'examen'@'localhost' IDENTIFIED BY 'examen'; --NB USUARIO - CONTRASEÑA (IDENTIFIED)
+GRANT ALL PRIVILEGES ON examen.* TO 'examen'@'localhost' WITH GRANT OPTION; --CONCEDER TODOS LOS PRIVILEGIOS AL USUARIO EXAMEN SOBRE LA BASE DE DATOS
+
+-- después de haber hecho lo anterior 
+USE examen;
+
+--Iniciar la tabla como paso final.
 CREATE TABLE logs (
     id int auto_increment PRIMARY KEY,
     navegador VARCHAR(255),
