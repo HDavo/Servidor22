d56d00b156042ef936f6730362714defa6fe9f70
pepe <pepe@pop-os.localdomain>
2023-03-26 03:22:33 +0200

    Sin crear en bbdd

--- python/basico/union_cosas/bbdd_interfaz.py
+++ python/basico/union_cosas/bbdd_interfaz.py
@@ -1,10 +1,13 @@
 import sqlite3
 from tkinter import messagebox
 
+# cuando se sepa solucionar la importación circular pasar todas las funciones de bbdd a este fichero
+
 #---------------------Conexión BBDD-----------------------------------
 def conexion_bbdd():
     # mi_conexion = sqlite3.connect("./python/basico/union_cosas/usuarios") 
-    """  esta línea funciona en el ordenador principal pero no dentro de la máquina. Sin poner la ruta de esta manera crea la bbdd dentro de la carpeta raiz abierta dentro del editor.
+    """  esta línea funciona en el ordenador principal pero no dentro de la máquina.
+    Sin poner la ruta de esta manera crea la bbdd dentro de la carpeta raiz abierta dentro del editor.
     """
 
     mi_conexion = sqlite3.connect("usuarios") # dentro de la máquina crea la base de datos dentro del directorio donde están el resto de ficheros
@@ -25,3 +28,17 @@ def conexion_bbdd():
         return messagebox.showinfo("BBDD", "BBDD creada con éxito")
     except:
         return messagebox.showwarning("¡Atención!", "La BDDD ya existe")
+    
+def crear():
+    mi_conexion = sqlite3.connect("usuarios")
+    
+    mi_cursor=mi_conexion.cursor()
+    
+    
+    #  mi_cursor.execute("INSERT INTO datos_usuarios VALUES (null,?,?,?,?,?)", variable_nombre.get(), variable_passwd.get(), variable_apellido.get(), variable_direccion.get(), cuadro_comentarios.get("1.0", END))
+    
+    mi_conexion.commit()
+    
+    
+    messagebox.showinfo("BBDD", "Registro insertado con éxito")
+    # mi_cursor.executemany("INSERT INTO productos VALUES (?,?,?,?)", productos) 
--- python/basico/union_cosas/interfaz_usuarios.py
+++ python/basico/union_cosas/interfaz_usuarios.py
@@ -1,15 +1,70 @@
 from tkinter import *
 from tkinter import messagebox
-import bbdd_interfaz, funcionalidad
+import sqlite3
+# import bbdd_interfaz, funcionalidad
 
 #---------------------Funcionalidad-----------------------------------
 # mirar si es posible ponerlo dentro de otro fichero sin que ocurra una importación circular
+
+#---------------------Conexión BBDD-----------------------------------
+def conexion_bbdd():
+    # mi_conexion = sqlite3.connect("./python/basico/union_cosas/usuarios") 
+    """  esta línea funciona en el ordenador principal pero no dentro de la máquina.
+    Sin poner la ruta de esta manera crea la bbdd dentro de la carpeta raiz abierta dentro del editor.
+    """
+
+    mi_conexion = sqlite3.connect("usuarios") # dentro de la máquina crea la base de datos dentro del directorio donde están el resto de ficheros
+    
+    mi_cursor = mi_conexion.cursor()
+    
+    try:
+        mi_cursor.execute('''
+        CREATE TABLE datos_usuarios (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre_usuario VARCHAR(50),
+        passwd VARCHAR(50),
+        apellido VARCHAR(50),
+        direccion VARCHAR(50),
+        comentarios VARCHAR(255))
+        ''')
+    
+        return messagebox.showinfo("BBDD", "BBDD creada con éxito")
+    except:
+        return messagebox.showwarning("¡Atención!", "La BDDD ya existe")
+    
+def crear():
+    mi_conexion = sqlite3.connect("usuarios")
+    
+    mi_cursor=mi_conexion.cursor()
+    
+    # mi_cursor.executemany("INSERT INTO datos_usuarios VALUES (null,?,?,?,?,?)", variable_nombre.get(), variable_passwd.get(), variable_apellido.get(), variable_direccion.get(), cuadro_comentarios.get("1.0", END)) #esta linea no funciona de esta manera
+    
+    mi_conexion.commit()
+    
+    
+    messagebox.showinfo("BBDD", "Registro insertado con éxito")
+    # mi_cursor.executemany("INSERT INTO productos VALUES (?,?,?,?)", productos) 
 
 def salir_aplicacion():
     valor = messagebox.askquestion("Salir", "¿Deseas salir de la aplicación?")
      
     if valor == "yes":
        raiz.destroy() # de momento dentro del mismo archivo porque necesita raiz
+       
+       
+       
+def limpieza_campos():
+    # para los entry se puede hacer de esta manera (siempre se debe aplicar set sobre la variable definida NUNCA SOBRE EL CAMPO)
+    variable_id.set("")
+    variable_nombre.set("")
+    variable_passwd.set("")
+    variable_apellido.set("")
+    variable_direccion.set("")
+    
+    
+    # para el campo de tipo text se debe hacer de la siguiente manera
+    cuadro_comentarios.delete(1.0, END) # esto significa que dentro del campo texto, borramos desde el primer caracter ala último que haya
+    
     
 
 raiz = Tk()
@@ -24,14 +79,14 @@ barra_menu = Menu(raiz)
 raiz.config(menu=barra_menu, width=400, height=400)
 
 menu_bbdd = Menu(barra_menu, tearoff=0)
-menu_bbdd.add_command(label="Conectar", command=bbdd_interfaz.conexion_bbdd)
+menu_bbdd.add_command(label="Conectar", command=conexion_bbdd)
 menu_bbdd.add_command(label="Salir", command=salir_aplicacion)
 
 menu_borrar = Menu(barra_menu, tearoff=0)
-menu_borrar.add_command(label="Borrar campos")
+menu_borrar.add_command(label="Borrar campos", command=limpieza_campos)
 
 menu_crud = Menu(barra_menu, tearoff=0)
-menu_crud.add_command(label="Crear")
+menu_crud.add_command(label="Crear", command=crear)
 menu_crud.add_command(label="Leer")
 menu_crud.add_command(label="Actualizar")
 menu_crud.add_command(label="Borrar")
@@ -85,21 +140,21 @@ comentarios_label.grid(row=5, column=0)
 
 # inputs o Entry
 
-cuadro_id = Entry(mi_frame)
+cuadro_id = Entry(mi_frame, textvariable=variable_id)
 cuadro_id.grid(row=0, column=1, padx=10, pady=10)
 cuadro_id.config(fg="red", justify="center")
 
-cuadro_nombre = Entry(mi_frame)
+cuadro_nombre = Entry(mi_frame, textvariable=variable_nombre)
 cuadro_nombre.grid(row=1, column=1, padx=10, pady=10)
 
-cuadro_passwd = Entry(mi_frame)
+cuadro_passwd = Entry(mi_frame, textvariable=variable_passwd)
 cuadro_passwd.grid(row=2, column=1, padx=10, pady=10)
 cuadro_passwd.config(show="*")
 
-cuadro_apellido = Entry(mi_frame)
+cuadro_apellido = Entry(mi_frame, textvariable=variable_apellido)
 cuadro_apellido.grid(row=3, column=1, padx=10, pady=10)
 
-cuadro_direccion = Entry(mi_frame)
+cuadro_direccion = Entry(mi_frame, textvariable=variable_direccion)
 cuadro_direccion.grid(row=4, column=1, padx=10, pady=10)
 
 cuadro_comentarios = Text(mi_frame, width=20, height=5)
@@ -116,7 +171,7 @@ otro_frame = Frame(raiz)
 otro_frame.pack()
 
 # botones
-boton_create = Button(otro_frame, text="Create")
+boton_create = Button(otro_frame, text="Create", command=crear)
 boton_create.grid(row=0, column=0, sticky="e", padx=10, pady=10)
 
 boton_read = Button(otro_frame, text="Read")
